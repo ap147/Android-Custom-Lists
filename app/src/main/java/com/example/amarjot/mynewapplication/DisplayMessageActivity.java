9d6ad1930e2b50565cb4afe4480a163844785f8a
Amarjot Parmar <amarjotparmar098@gmail.com>
2018-08-11 13:28:44 +1200

    back animation added (RecipeDetails to HomePage)

--- app/src/main/java/com/example/amarjot/mynewapplication/DisplayMessageActivity.java
+++ app/src/main/java/com/example/amarjot/mynewapplication/DisplayMessageActivity.java
@@ -4,16 +4,21 @@ import android.graphics.Color;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.mikhaellopez.circularimageview.CircularImageView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,12 +49,23 @@ public class DisplayMessageActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        toolbar.setTitleTextColor(Color.WHITE);
+        getSupportActionBar().setTitle(recipe_title);
+
         ActionBar actionbar = getSupportActionBar();
         actionbar.setDisplayHomeAsUpEnabled(true);
         actionbar.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24px);
+    }
 
-        toolbar.setTitleTextColor(Color.WHITE);
-        getSupportActionBar().setTitle(recipe_title);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                overridePendingTransition(R.anim.slide_out_left, R.anim.slide_in_left);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
